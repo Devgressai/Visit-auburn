ba34ae7ea74aec5305dad2a35549c73c3cb3a864
Devgressai <229847365+Devgressai@users.noreply.github.com>
2025-12-26 23:32:49 -0800

    Crop hero image to avoid dark right side

--- components/homepage/CinematicHero.tsx
+++ components/homepage/CinematicHero.tsx
@@ -51,6 +51,7 @@ export function CinematicHero({
           fill
           priority
           className={`object-cover transition-transform duration-[2s] ${isLoaded ? 'scale-100' : 'scale-110'}`}
+          style={{ objectPosition: '30% center' }}
         />
       </div>
 
